dac65559eca4cf60496485ae3ea74e0667c148b9
elegantinteriorconst02 <elegantinteriorconst02@gmail.com>
2026-02-09 18:06:29 +0530

    Update project-store.js

--- projects/data/project-store.js
+++ projects/data/project-store.js
@@ -1,1 +1,30 @@
 
+/* ===============================
+   PROJECT STORAGE (localStorage)
+================================ */
+const PROJECT_KEY = "projects";
+
+function getProjects(){
+  return JSON.parse(localStorage.getItem(PROJECT_KEY) || "[]");
+}
+
+function saveProjects(list){
+  localStorage.setItem(PROJECT_KEY, JSON.stringify(list));
+}
+
+function addProject(project){
+  const list = getProjects();
+  list.push(project);
+  saveProjects(list);
+}
+
+function updateProject(id, data){
+  const list = getProjects().map(p =>
+    p.id === id ? { ...p, ...data } : p
+  );
+  saveProjects(list);
+}
+
+function getProjectById(id){
+  return getProjects().find(p => p.id === id);
+}
