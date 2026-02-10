c130df2ff4af2edabc3e4b916eac40024cf79a12
elegantinteriorconst02 <elegantinteriorconst02@gmail.com>
2026-02-10 12:24:19 +0530

    Update projects.js

--- projects/data/projects.js
+++ projects/data/projects.js
@@ -1,30 +1,6 @@
-
-/* ===============================
-   PROJECT STORAGE (localStorage)
-================================ */
-const PROJECT_KEY = "projects";
-
 function getProjects(){
-  return JSON.parse(localStorage.getItem(PROJECT_KEY) || "[]");
+  return JSON.parse(localStorage.getItem("projects")||"[]");
 }
-
-function saveProjects(list){
-  localStorage.setItem(PROJECT_KEY, JSON.stringify(list));
-}
-
-function addProject(project){
-  const list = getProjects();
-  list.push(project);
-  saveProjects(list);
-}
-
-function updateProject(id, data){
-  const list = getProjects().map(p =>
-    p.id === id ? { ...p, ...data } : p
-  );
-  saveProjects(list);
-}
-
-function getProjectById(id){
-  return getProjects().find(p => p.id === id);
+function saveProjects(p){
+  localStorage.setItem("projects",JSON.stringify(p));
 }
